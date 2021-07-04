9364d5188aa4adf952fe14f3b4dfab408b946a8d
Djuned Fernando Djusdek <santensuru@users.noreply.github.com>
2021-07-04 11:02:46 +0900

    add missing items
    favicon (using serve-favicon)
    bootstrap css/js min map

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 var express = require('express')
   , stylus = require('stylus')
   , nib = require('nib')
+  , favicon = require('serve-favicon')
 
 
 var app = express()
@@ -23,6 +24,8 @@ app.use(stylus.middleware(
   }
 ))
 app.use(express.static(__dirname + '/public'))
+
+app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 
 app.get('/', function (req, res) {
   res.render('index',
